d39bd6f7a1e4df6274a7f60ec6462c0f66dd136b
MrGoodBytes <bytes@goodbytes.live>
2020-11-17 11:04:33 -0600

    Added EnumParameterConverter

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -4,6 +4,7 @@
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 
+use Bytes\EnumSerializerBundle\Request\EnumParameterConverter;
 use Bytes\EnumSerializerBundle\Serializer\Normalizer\EnumNormalizer;
 
 /**
@@ -16,5 +17,10 @@ return static function (ContainerConfigurator $container) {
     $services->set('bytesenumserializerbundle.enumnormalizer', EnumNormalizer::class)
         ->tag('serializer.normalizer')
         ->args([service('serializer.normalizer.object')]);
+
+    $services->set('bytesenumserializerbundle.enum_parameter_converter', EnumParameterConverter::class)
+        ->tag('request.param_converter', [
+            'converter' => 'bytes_enum'
+        ]);
 
 };
