7d7676f1464c4f7fd609b850eb7a3d912ead8e44
MrGoodBytes <bytes@goodbytes.live>
2022-01-04 10:21:49 -0600

    Add some tests

--- Tests/ChildTest.php
+++ Tests/ChildTest.php
@@ -9,10 +9,30 @@ use Bytes\EnumSerializerBundle\Tests\Fixtures\PlainInheritedEnum;
 
 class ChildTest extends TestSerializationCase
 {
+    /**
+     * @return void
+     */
     public function testIsValidFunction()
     {
         $this->assertTrue(PlainInheritedEnum::isValid('channelCreate'));
         $this->assertFalse(PlainInheritedEnum::isValid('abcdefg'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testMake() {
+        $this->assertEquals(PlainInheritedEnum::from('channelCreate'), PlainInheritedEnum::make('channelCreate'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testDeprecatedGetToFunctions() {
+        $this->assertCount(2, PlainInheritedEnum::getLabels());
+        $this->assertCount(2, PlainInheritedEnum::toLabels());
+        $this->assertCount(2, PlainInheritedEnum::getValues());
+        $this->assertCount(2, PlainInheritedEnum::toValues());
     }
 
     /**
