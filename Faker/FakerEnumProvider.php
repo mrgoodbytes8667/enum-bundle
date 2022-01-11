b3492a3ef19f7b8751d111db0525fd4075121e20
MrGoodBytes <bytes@goodbytes.live>
2022-01-11 15:11:02 -0600

    Updated FakerEnumProvider

--- Faker/FakerEnumProvider.php
+++ Faker/FakerEnumProvider.php
@@ -2,41 +2,48 @@
 
 namespace Bytes\EnumSerializerBundle\Faker;
 
-use Spatie\Enum\Enum;
+use BackedEnum;
+use Faker\Provider\Base;
+use UnitEnum;
 
-class FakerEnumProvider extends \Spatie\Enum\Faker\FakerEnumProvider
+class FakerEnumProvider extends Base
 {
     /**
      * A random instance of the enum you pass in.
      *
-     * @param class-string<\Bytes\EnumSerializerBundle\Enums\Enum> $enum
+     * @param class-string<UnitEnum|BackedEnum> $enum
      *
-     * @return Enum
+     * @return UnitEnum|BackedEnum
      */
-    public function randomEnum(string $enum): Enum
+    public function randomEnum(string $enum): UnitEnum|BackedEnum
     {
-        if (! is_subclass_of($enum, \Bytes\EnumSerializerBundle\Enums\Enum::class)) {
-            throw new InvalidArgumentException(sprintf(
-                'You have to pass the FQCN of a "%s" class but you passed "%s".',
-                Enum::class,
-                $enum
-            ));
-        }
+        return static::randomElement($enum::cases());
+    }
 
-        return $enum::make(static::randomElement($enum::tempToValues()));
+    /**
+     * A random value of the enum you pass in.
+     *
+     * @param class-string<BackedEnum> $enum
+     *
+     * @return string|int
+     */
+    public function randomEnumValue(string $enum): int|string
+    {
+        return $this->randomEnum($enum)->value;
     }
 
     /**
      * A random label of the enum you pass in.
-     * @param string $enum
+     *
+     * @param class-string<BackedEnum> $enum
+     * @deprecated since 1.6.1, this method is deprecated, there is no replacement.
      *
      * @return string
-     * @deprecated since 1.7.0, this method is deprecated, there is no replacement.
-     *
      */
     public function randomEnumLabel(string $enum): string
     {
-        trigger_deprecation('mrgoodbytes8667/enum-serializer-bundle', '1.7.0', 'Using "%s" is deprecated, there is no replacement.', __METHOD__);
-        return parent::randomEnumLabel($enum);
+        trigger_deprecation('mrgoodbytes8667/enum-serializer-bundle', '1.6.1', 'Using "%s" is deprecated, there is no replacement.', __METHOD__);
+        return $this->randomEnumValue($enum);
     }
+
 }
