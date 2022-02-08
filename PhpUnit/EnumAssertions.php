bc993e8ebbf583299bf1843a47ed49bd1f356ab8
MrGoodBytes <bytes@goodbytes.live>
2022-02-08 12:19:43 -0600

    Adding initial EnumAssertions

---
+++ PhpUnit/EnumAssertions.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Bytes\EnumSerializerBundle\PhpUnit;
+
+use BackedEnum;
+use BadMethodCallException;
+use InvalidArgumentException;
+use PHPUnit\Framework\Assert as PHPUnit;
+use TypeError;
+
+abstract class EnumAssertions
+{
+    /**
+     * Checks if actual is a value of the given enum class name.
+     *
+     * @param string $enum
+     * @psalm-param class-string<BackedEnum> $enum
+     * @param string|mixed $actual
+     * @param string|null $message
+     */
+    public static function assertIsEnumValue(string $enum, $actual, ?string $message = null): void
+    {
+        if (!is_subclass_of($enum, BackedEnum::class)) {
+            throw new InvalidArgumentException(sprintf('The `$enum` argument has to be a FQCN to an `%s` class', BackedEnum::class));
+        }
+
+        PHPUnit::assertContains(
+            $actual,
+            forward_static_call([$enum, 'toValues']),
+            $message ?? ''
+        );
+    }
+
+    /**
+     * Checks if actual is a label of the given enum class name.
+     *
+     * @param string $enum
+     * @psalm-param class-string<BackedEnum> $enum
+     * @param string|mixed $actual
+     * @param string|null $message
+     */
+    public static function assertIsEnumLabel(string $enum, $actual, ?string $message = null): void
+    {
+        if (!is_subclass_of($enum, BackedEnum::class)) {
+            throw new InvalidArgumentException(sprintf('The `$enum` argument has to be a FQCN to an `%s` class', BackedEnum::class));
+        }
+
+        PHPUnit::assertContains(
+            $actual,
+            forward_static_call([$enum, 'toLabels']),
+            $message ?? ''
+        );
+    }
+
+    /**
+     * Checks if actual (after being transformed to enum) equals expected.
+     *
+     * @param BackedEnum $expected
+     * @param BackedEnum|string|int|mixed $actual
+     * @param string|null $message
+     */
+    public static function assertEqualsEnum(BackedEnum $expected, $actual, ?string $message = null): void
+    {
+        try {
+            $enum = static::asEnum($actual, $expected);
+
+            PHPUnit::assertTrue(
+                $expected->equals($enum),
+                $message ?? ''
+            );
+        } catch (TypeError $ex) {
+            PHPUnit::assertTrue(false, $ex->getMessage());
+        } catch (BadMethodCallException $ex) {
+            PHPUnit::assertTrue(false, $ex->getMessage());
+        }
+    }
+
+    /**
+     * @param int|string|BackedEnum $value
+     * @param BackedEnum $enum
+     *
+     * @return BackedEnum
+     *
+     * @throws TypeError
+     * @throws BadMethodCallException
+     *
+     * @see \BackedEnum::make()
+     */
+    protected static function asEnum($value, BackedEnum $enum): BackedEnum
+    {
+        if ($value instanceof BackedEnum) {
+            return $value;
+        }
+
+        return forward_static_call(
+            [get_class($enum), 'make'],
+            $value
+        );
+    }
+
+    /**
+     * Checks if actual equals expected.
+     *
+     * @param BackedEnum $expected
+     * @param BackedEnum|mixed $actual
+     * @param string|null $message
+     */
+    public static function assertSameEnum(BackedEnum $expected, $actual, ?string $message = null): void
+    {
+        static::assertIsEnum($actual);
+
+        PHPUnit::assertTrue(
+            $expected->equals($actual),
+            $message ?? ''
+        );
+    }
+
+    /**
+     * Checks if actual extends \BackedEnum::class.
+     *
+     * @param BackedEnum|mixed $actual
+     * @param string|null $message
+     */
+    public static function assertIsEnum($actual, ?string $message = null): void
+    {
+        PHPUnit::assertInstanceOf(
+            BackedEnum::class,
+            $actual,
+            $message ?? ''
+        );
+    }
+
+    /**
+     * Checks if actual equals the value of expected.
+     *
+     * @param BackedEnum $expected
+     * @param string|int|mixed $actual
+     * @param string|null $message
+     */
+    public static function assertSameEnumValue(BackedEnum $expected, $actual, ?string $message = null): void
+    {
+        PHPUnit::assertSame(
+            $expected->value,
+            $actual,
+            $message ?? ''
+        );
+    }
+}
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/mrgoodbytes8667/enum-serializer-bundle/release?label=stable&logo=github&logoColor=FFF&style=flat)
 ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/mrgoodbytes8667/enum-serializer-bundle/tests?logo=github&logoColor=FFF&style=flat)
 [![codecov](https://img.shields.io/codecov/c/github/mrgoodbytes8667/enum-serializer-bundle?logo=codecov&logoColor=FFF&style=flat)](https://codecov.io/gh/mrgoodbytes8667/enum-serializer-bundle)  
-A bundle to provide some helper methods for PHP 8.1+ enums
+A bundle to provide some helper methods for PHP 8.1+ enums inspired by [spatie/enum](https://github.com/spatie/enum)
 
 ## Upgrading to 2.0
 Change all classes that extend `Bytes\EnumSerializerBundle\Enums\Enum` to be string backed enums, using the new
