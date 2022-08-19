cc6aa85f89ac90eedbaed16dfb40bfd1fec9d5e8
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2022-08-19 12:54:44 -0500

    Added deprecations for 3.0 (#21)

--- Enums/EasyAdminChoiceEnumInterface.php
+++ Enums/EasyAdminChoiceEnumInterface.php
@@ -3,11 +3,13 @@
 namespace Bytes\EnumSerializerBundle\Enums;
 
 use BackedEnum;
+use JetBrains\PhpStorm\Deprecated;
 
 interface EasyAdminChoiceEnumInterface extends BackedEnum
 {
     /**
      * @return array<string>
      */
+    #[Deprecated('since 2.2, use provideFormChoices()', '%class%::provideFormChoices()')]
     public static function easyAdminChoices(): array;
 }
