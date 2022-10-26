5eb17842785c31fc20f7a6e75544953bd65c765f
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2022-10-26 09:29:09 -0500

    Create non-static class method formChoiceKey() in FormChoiceEnumInterface (#27)

--- Enums/FormChoiceEnumInterface.php
+++ Enums/FormChoiceEnumInterface.php
@@ -4,6 +4,7 @@ namespace Bytes\EnumSerializerBundle\Enums;
 
 use BackedEnum;
 use UnitEnum;
+use function Symfony\Component\String\u;
 
 interface FormChoiceEnumInterface extends BackedEnum
 {
@@ -11,6 +12,11 @@ interface FormChoiceEnumInterface extends BackedEnum
      * @return array<string>
      */
     public static function provideFormChoices(): array;
+
+    /**
+     * @return string
+     */
+    public function formChoiceKey(): string;
 
     /**
      * @param UnitEnum $value
