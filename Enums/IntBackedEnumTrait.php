d28b5f77b8aec6fda0499755a2a265c26089a675
mrgoodbytes8667 <72107446+mrgoodbytes8667@users.noreply.github.com>
2022-12-14 10:15:53 -0600

    Add tryNormalizeToEnum()/tryNormalizeToValue() methods (#32)

--- Enums/IntBackedEnumTrait.php
+++ Enums/IntBackedEnumTrait.php
@@ -38,4 +38,32 @@ trait IntBackedEnumTrait
 
         return static::from($value);
     }
+
+    /**
+     * @param BackedEnum|int $value
+     * @return int|null
+     */
+    public static function tryNormalizeToValue(BackedEnum|int $value): ?int
+    {
+        if ($value instanceof static) {
+            return $value->value;
+        } elseif (is_int($value) && !is_null(static::tryFrom($value))) {
+            return $value;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param BackedEnum|int $value
+     * @return static|null
+     */
+    public static function tryNormalizeToEnum(BackedEnum|int $value): ?static
+    {
+        if ($value instanceof static) {
+            return $value;
+        }
+
+        return static::tryFrom($value);
+    }
 }
--- Enums/StringBackedEnumTrait.php
+++ Enums/StringBackedEnumTrait.php
@@ -38,4 +38,33 @@ trait StringBackedEnumTrait
 
         return static::from($value);
     }
+
+
+    /**
+     * @param BackedEnum|string $value
+     * @return string|null
+     */
+    public static function tryNormalizeToValue(BackedEnum|string $value): ?string
+    {
+        if ($value instanceof static) {
+            return $value->value;
+        } elseif (is_string($value) && !is_null(static::tryFrom($value))) {
+            return $value;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param BackedEnum|string $value
+     * @return static|null
+     */
+    public static function tryNormalizeToEnum(BackedEnum|string $value): ?static
+    {
+        if ($value instanceof static) {
+            return $value;
+        }
+
+        return static::tryFrom($value);
+    }
 }
